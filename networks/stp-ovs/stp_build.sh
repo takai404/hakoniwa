1cb7b1958809e3b3bae8b297dae3ce229af11d3b
takai <106087407+takai404@users.noreply.github.com>
2022-07-22 21:10:40 +0900

    modify comments

--- networks/stp-ovs/stp_build.sh
+++ networks/stp-ovs/stp_build.sh
@@ -20,7 +20,7 @@ ip link add access2_e2 type veth peer name eth0 netns pc2
 ip link add access3_e2 type veth peer name eth0 netns pc3
 ip link add access4_e2 type veth peer name eth0 netns pc4
 
-# set IP address: core1
+# create node: core1
 #   create openvswitch bridge
 ovs-vsctl add-br core1
 #   create openvswitch ports
@@ -29,7 +29,7 @@ ovs-vsctl add-port core1 core1_e1
 ovs-vsctl add-port core1 core1_e2
 ovs-vsctl set Bridge core1 stp_enable=true
 
-# set IP address: core2
+# create node: core2
 #   create openvswitch bridge
 ovs-vsctl add-br core2
 #   create openvswitch ports
@@ -38,7 +38,7 @@ ovs-vsctl add-port core2 core2_e1
 ovs-vsctl add-port core2 core2_e2
 ovs-vsctl set Bridge core2 stp_enable=true
 
-# set IP address: access1
+# create node: access1
 #   create openvswitch bridge
 ovs-vsctl add-br access1
 #   create openvswitch ports
@@ -47,7 +47,7 @@ ovs-vsctl add-port access1 access1_e0
 ovs-vsctl add-port access1 access1_e2
 ovs-vsctl set Bridge access1 stp_enable=true
 
-# set IP address: access2
+# create node: access2
 #   create openvswitch bridge
 ovs-vsctl add-br access2
 #   create openvswitch ports
@@ -56,7 +56,7 @@ ovs-vsctl add-port access2 access2_e0
 ovs-vsctl add-port access2 access2_e2
 ovs-vsctl set Bridge access2 stp_enable=true
 
-# set IP address: access3
+# create node: access3
 #   create openvswitch bridge
 ovs-vsctl add-br access3
 #   create openvswitch ports
@@ -65,7 +65,7 @@ ovs-vsctl add-port access3 access3_e0
 ovs-vsctl add-port access3 access3_e2
 ovs-vsctl set Bridge access3 stp_enable=true
 
-# set IP address: access4
+# create node: access4
 #   create openvswitch bridge
 ovs-vsctl add-br access4
 #   create openvswitch ports
@@ -74,19 +74,19 @@ ovs-vsctl add-port access4 access4_e0
 ovs-vsctl add-port access4 access4_e2
 ovs-vsctl set Bridge access4 stp_enable=true
 
-# set IP address: pc1
+# create node: pc1
 ip netns exec pc1 sysctl -w net.ipv4.icmp_ratemask=0
 ip netns exec pc1 ip addr add 10.0.0.1/24 dev eth0
 
-# set IP address: pc2
+# create node: pc2
 ip netns exec pc2 sysctl -w net.ipv4.icmp_ratemask=0
 ip netns exec pc2 ip addr add 10.0.0.2/24 dev eth0
 
-# set IP address: pc3
+# create node: pc3
 ip netns exec pc3 sysctl -w net.ipv4.icmp_ratemask=0
 ip netns exec pc3 ip addr add 10.0.0.3/24 dev eth0
 
-# set IP address: pc4
+# create node: pc4
 ip netns exec pc4 sysctl -w net.ipv4.icmp_ratemask=0
 ip netns exec pc4 ip addr add 10.0.0.4/24 dev eth0
 
